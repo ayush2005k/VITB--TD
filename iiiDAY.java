7c30891293adeba76060bbb111f940776d4ae40f
ayush singh <145686342+ayush2005k@users.noreply.github.com>
2025-07-27 14:46:40 +0530

    Valid Palindrome
    LC-125(Valid Palindrome--String)
    
    PROBLEM STATEMENT :
    
    A phrase is a palindrome if, after converting all uppercase letters into lowercase letters and removing all non-alphanumeric characters, it reads the same forward and backward. Alphanumeric characters include letters and numbers.
    
    Given a string s, return true if it is a palindrome, or false otherwise.

---
+++ iiiDAY.java
@@ -0,0 +1,32 @@
+public class iiiDAY {
+    public static void main(String[] args) {
+        String pan = "A man, a plan, a canal: Panama";
+        String lowerpan = pan.toLowerCase();
+        System.out.println(lowerpan);
+        String cleanlowepan = lowerpan.replaceAll("[^a-zA-Z0-9]",""); // need to watch the video on this 
+        System.out.println(cleanlowepan);
+        if (cleanlowepan!="") {
+            System.out.println("True");
+        }else{
+            System.out.println("flase");
+        }
+    }
+}
+
+// different methods
+ class Solution {
+    public boolean isPalindrome(String param_1) {
+        String cleanedString = param_1.toLowerCase().replaceAll("[^a-zA-Z0-9]", "");
+        int left = 0;
+        int right = cleanedString.length() - 1;
+
+        while (left < right) {
+            if (cleanedString.charAt(left) != cleanedString.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+}
