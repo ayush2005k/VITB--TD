ede351ec83521c072cdbe4d4c4d78bae82fab08a
ayush singh <145686342+ayush2005k@users.noreply.github.com>
2025-07-28 10:59:22 +0530

    D-4) Two Sum
    PROBLEM STATEMENT:
    
    Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
    
    You may assume that each input would have exactly one solution, and you may not use the same element twice.
    
    You can return the answer in any order.
    
     
    
    Example 1:
    
    Input: nums = [2,7,11,15], target = 9
    Output: [0,1]
    Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
    Example 2:
    
    Input: nums = [3,2,4], target = 6
    Output: [1,2]
    Example 3:
    
    Input: nums = [3,3], target = 6
    Output: [0,1]

---
+++ ivDAY.java
@@ -0,0 +1,17 @@
+public class ivDAY {
+    public static void main(String[] args) {
+        int[] num = {2, 7, 11, 15};
+        int target = 9; // Fix: Change target to a single integer.
+
+        for (int i = 0; i < num.length; i++) {
+            // Fix: Start the inner loop from 'i + 1' for better efficiency and to avoid the 'if(i < j)' check.
+            for (int j = i + 1; j < num.length; j++) { //j=i+j to remove dupcliate we can j=o but then if(i<j)
+                int sum = num[i] + num[j];
+
+                if (sum <= target) { // The comparison now works correctly.
+                    System.out.println("Pair(" + num[i] + ", " + num[j] + ") sum: " + sum);
+                }
+            }
+        }
+    }
+}
