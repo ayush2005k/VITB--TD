5945e5a41111700be9032376d2d11ee737f53232
ayush singh <145686342+ayush2005k@users.noreply.github.com>
2025-08-01 01:54:05 +0530

    ivDAY.java

--- ivDAY.java
+++ ivDAY.java
@@ -1,8 +1,7 @@
 public class ivDAY {
     public static void main(String[] args) {
         int[] num = {2, 7, 11, 15};
-        int target = 9; // Fix: Change target to a single integer.
-
+        int target = 9; 
         for (int i = 0; i < num.length; i++) {
             // Fix: Start the inner loop from 'i + 1' for better efficiency and to avoid the 'if(i < j)' check.
             for (int j = i + 1; j < num.length; j++) { //j=i+j to remove dupcliate we can j=o but then if(i<j)
